2172149ead04dd76178b8b8f7b38b674adf977c0
Janek Karaszewski <jakarwski19@gmail.com>
2021-01-18 10:10:55 +0100

    New graphics and logo

--- src/main/java/app/gui/labels/LogoPanel.java
+++ src/main/java/app/gui/labels/LogoPanel.java
@@ -5,7 +5,9 @@ import java.awt.*;
 
 public class LogoPanel extends JLabel {
     public LogoPanel() {
-        setOpaque(true);
-        setBackground(Color.black);
+        super("", CENTER);
+        setPreferredSize(new Dimension(600, 200));
+        ImageIcon logo = new ImageIcon(getClass().getResource("/img/logo.png"));
+        setIcon(logo);
     }
 }
